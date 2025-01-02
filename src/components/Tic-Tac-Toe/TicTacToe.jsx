28316cda1fceaed9c65f3c4ed08f1c92c7846399
uiux@okdollar frontend <uiux@okdollar.com>
2025-01-02 16:03:59 +0630

    finish project

--- src/components/Tic-Tac-Toe/TicTacToe.jsx
+++ src/components/Tic-Tac-Toe/TicTacToe.jsx
@@ -2,11 +2,11 @@ import 'react'
 import './TicTacToe.css';
 import circle_icon from '../../assets/circle.png';
 import cross_icon from '../../assets/cross.png';
-import { useState } from 'react';
+import { useState,useRef } from 'react';
 
 const TicTacToe = () => {
-
-  const data = ['','','','','','','','',''];
+  const titleRef = useRef(null);
+  const [data,setData] = useState(['','','','','','','','',''])
 
   const [lock,setLock] = useState(false)
   const [count,setCount] = useState(0)
@@ -15,28 +15,71 @@ const TicTacToe = () => {
     if(lock){
       return 0;
     }
-    if(count%2 === 0){
-      e.target.innerHTML = `<img src=${cross_icon} alt='cross' />`
-      data[number]= 'X';
-      setCount(count+1)
-    }else{
-      e.target.innerHTML = `<img src=${circle_icon} alt='circle' />`
-      data[number]= 'O';
-      setCount(count+1)
+    if(data[number] ===''){
+      const newData = [...data];
+      if(count%2==0){
+        newData[number] = 'X';
+        e.target.innerHTML = `<img src=${cross_icon} alt='cross' />`
+       
+      }else{
+        newData[number] = 'O';
+        e.target.innerHTML = `<img src=${circle_icon} alt='circle' />`
+      }
+      setData(newData); 
+      setCount(count + 1);
+      checkWin(newData);
     }
   }
 
 
-  const checkWin = ()=>{
-    if(data[0]=== data[1] && data[1]===data[2] && data[0]!==''){
-      setLock(true)
-    }else if(data[3]=== dat)
+
+    const checkWin = (newData) => {
+      // Same winning conditions ** this point should be known **
+      const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+  
+      for (let condition of winningConditions) {
+        console.log(condition);
+        const [a, b, c] = condition;
+        if (newData[a] && newData[a] === newData[b] && newData[b] === newData[c]) {
+          won(newData[a]);
+          return;
+        }
+      }
+    };
+
+  const won = (winner)=>{
+    setLock(true);
+    if(winner === 'X'){
+      titleRef.current.innerHTML = 'Player 1 won the Game';
+    }else{
+      titleRef.current.innerHTML = 'Player 2 won the Game';
+    }
   }
+
+  const resetGame = () => {
+    setData(['', '', '', '', '', '', '', '', '']);
+    setCount(0);
+    setLock(false);
+    titleRef.current.innerHTML = 'Tic Tac Toe Game in React';
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box) => {
+      box.innerHTML = ''; 
+    });
+  };
 
 
   return (
-    <div className="container">
-      <h2 className='text'>Tic Tac Toe Game in React</h2>
+    <div className ='container'>
+      <h2 className='text' ref={titleRef}>Tic Tac Toe Game in React</h2>
       <div className='boxContainer'>
         <div className='row'>
           <div className='box' onClick={(e)=>boxClick(e,0)}></div>
@@ -55,7 +98,7 @@ const TicTacToe = () => {
         </div>
       </div> 
       <div className='btnContainer'>
-        <button className='btn'>Reset</button>
+        <button className='btn' onClick={resetGame}>Reset</button>
       </div>
     </div>
   )
